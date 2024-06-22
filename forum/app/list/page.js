fc7ed16043c65723507cfeff402108234e8eaec7
umingming <u_0112@naver.com>
2024-06-22 14:31:45 +0900

    feat: 상세 페이지로 이동

--- forum/app/list/page.js
+++ forum/app/list/page.js
@@ -1,5 +1,6 @@
 import { findPostAll } from "@/utill/database";
 import Link from "next/link";
+import DetailLink from "./detailLink";
 
 export default async function List() {
     const posts = await findPostAll();
@@ -20,9 +21,11 @@ function Post({ id, title, content }) {
     const url = `/detail/${id}`;
     return (
         <div className="list-item">
+            {/* Link태그엔 Router prefetch 기능 내장 */}
             <Link href={url}>
                 <h4>{title}</h4>
             </Link>
+            <DetailLink id={id} />
             <p>{content}</p>
         </div>
     );
