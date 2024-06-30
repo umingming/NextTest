6e1d9291193e8e8ab67c90ccecedee82a93b32c7
umingming <u_0112@naver.com>
2024-06-30 12:57:27 +0900

    feat: Ajax 활용한 삭제 기능 구현

--- forum/app/list/listItem.js
+++ forum/app/list/listItem.js
@@ -5,6 +5,20 @@ import ButtonLink from "./buttonLink";
 
 export default function ListItem({ id, title, content }) {
     const url = `/detail/${id}`;
+
+    function deletePost({ target }) {
+        const body = JSON.stringify({ id });
+        fetch("/api/post", { method: "DELETE", body })
+            .then((result) => result.json())
+            .then(() => {
+                const $item = target.closest(".list-item");
+                $item.style.opacity = 0;
+                setTimeout(() => {
+                    $item.style.display = "none";
+                }, 1000);
+            });
+    }
+
     return (
         <div className="list-item">
             {/* Link태그엔 Router prefetch 기능 내장 */}
@@ -12,15 +26,7 @@ export default function ListItem({ id, title, content }) {
                 <h4>{title}</h4>
             </Link>
             <ButtonLink id={id} type="edit" />
-            <button
-                onClick={() => {
-                    fetch("/api/post", { method: "DELETE", body: id })
-                        .then((result) => result.json())
-                        .then(console.log);
-                }}
-            >
-                Delete
-            </button>
+            <button onClick={deletePost}>Delete</button>
             <p>{content}</p>
         </div>
     );
--- forum/pages/api/post.js
+++ forum/pages/api/post.js
@@ -1,21 +1,24 @@
 import { getPostCollection } from "@/utill/database";
 import { ObjectId } from "mongodb";
 
-export default async function handler({ method, body, params }, response) {
+export default async function handler(
+    { method, body, params, query },
+    response,
+) {
     if (method === "POST") {
         try {
             const { id, title, content } = body;
             if (!title) {
                 return response.status(400).json("제목 써라");
             }
-            const collection = await getPostCollection();
-            await collection.updateOne(
-                { _id: new ObjectId(id) },
-                { $set: { title, content } },
-            );
             // const collection = await getPostCollection();
-            // const { insertedId } = await collection.insertOne(body);
-            return response.status(200).redirect(`/detail/${id}`);
+            // await collection.updateOne(
+            //     { _id: new ObjectId(id) },
+            //     { $set: { title, content } },
+            // );
+            const collection = await getPostCollection();
+            const { insertedId } = await collection.insertOne(body);
+            return response.status(200).redirect(`/detail/${insertedId}`);
         } catch (error) {
             return response.status(500).json("에러났다.");
         }
@@ -28,8 +31,10 @@ export default async function handler({ method, body, params }, response) {
         }
     } else if (method === "DELETE") {
         try {
+            console.log(query);
             const collection = await getPostCollection();
-            await collection.deleteOne({ _id: new ObjectId(body) });
+            const { id } = JSON.parse(body);
+            await collection.deleteOne({ _id: new ObjectId(id) });
             return response.status(200).redirect("list");
         } catch (error) {
             return response.status(500).json("에러났다.");
