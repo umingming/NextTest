912795e6ab3cab2367581ce4f4fc47c8aa22f1d4
umingming <u_0112@naver.com>
2024-09-19 13:01:07 +0900

    Css 설정

--- carrot-market/app/page.tsx
+++ carrot-market/app/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-    return <div></div>;
+    return (
+        // space는 자식 요소에 margin을 만들어줌.
+        <div className="bg-slate-400 py-10 px-5 flex flex-col space-y-5">
+            <div className="bg-white p-10 rounded-2xl"></div>
+            <div className="bg-white p-10 rounded-2xl"></div>
+            <div className="bg-white p-10 rounded-2xl"></div>
+            <div className="bg-white p-10 rounded-2xl"></div>
+            <div className="bg-white p-10 rounded-2xl"></div>
+        </div>
+    );
 }
