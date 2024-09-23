7b256686862a9c2dc7088205b474f440da107785
umingming <u_0112@naver.com>
2024-09-23 20:20:27 +0900

    feat: Add enter page

--- carrot-market/app/layout.tsx
+++ carrot-market/app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className="dark">{children}</body>
+            <body>{children}</body>
         </html>
     );
 }
