c5b804123c67d7db4f7868fd880446392beaaf7c
umingming <u_0112@naver.com>
2024-10-18 18:49:08 +0900

    feat: Handle form to submit

--- carrot-market/app/products/create/page.tsx
+++ carrot-market/app/products/create/page.tsx
@@ -1,33 +1,47 @@
+"use client";
+
 import ButtonText from "@/components/common/button/ButtonText";
 import IconBase from "@/components/common/icon/IconBase";
 import InputBox from "@/components/common/input/InputBox";
 import InputPrice from "@/components/common/input/InputPrice";
 import InputText from "@/components/common/input/InputText";
 import { ICON_KEY } from "@/constants/keyConstants";
+import useMutation from "@/libs/client/hooks/useMutation";
+import { useForm } from "react-hook-form";
 
-export default function ItemCreate() {
+interface CreateProductForm {
+    name: string;
+    price: number;
+    description: string;
+    image?: string;
+}
+
+export default function CreateProduct() {
+    const { register, handleSubmit } = useForm<CreateProductForm>();
+    const [createProduct] = useMutation("/api/products");
+    const onValid = (data: CreateProductForm) => createProduct(data);
+
     return (
         <div className="space-y-5 px-4 py-4">
-            <div>
-                <label className="flex h-48 w-full items-center justify-center rounded-md border-2 border-dashed border-gray-300 py-6 text-gray-600 hover:border-orange-500 hover:text-orange-500">
-                    <IconBase
-                        iconKey={ICON_KEY.IMAGE}
-                        size={12}
-                        strokeWidth={1}
-                    />
-                    <input className="hidden" type="file" />
-                </label>
-            </div>
-            <div>
-                <InputText label="Name" />
-            </div>
-            <div className="my-5">
-                <InputPrice />
-            </div>
-            <div>
-                <InputBox label="Description" />
-            </div>
-            <ButtonText label="Upload item" />
+            <form onSubmit={handleSubmit(onValid)}>
+                <div className="mb-5">
+                    <label className="flex h-48 w-full items-center justify-center rounded-md border-2 border-dashed border-gray-300 py-6 text-gray-600 hover:border-orange-500 hover:text-orange-500">
+                        <IconBase
+                            iconKey={ICON_KEY.IMAGE}
+                            size={12}
+                            strokeWidth={1}
+                        />
+                        <input className="hidden" type="file" />
+                    </label>
+                </div>
+                <InputText register={register("name")} label="Name" />
+                <InputPrice register={register("price")} />
+                <InputBox
+                    register={register("description")}
+                    label="Description"
+                />
+                <ButtonText label="Upload item" />
+            </form>
         </div>
     );
 }
--- carrot-market/components/common/input/InputBox.tsx
+++ carrot-market/components/common/input/InputBox.tsx
@@ -2,14 +2,15 @@ import { InputProps } from "@/types/input";
 import { useId } from "react";
 import InputBase from "./InputBase";
 
-export default function InputBox({ label, placeholder }: InputProps) {
+export default function InputBox({ label, placeholder, register }: InputProps) {
     const id = useId();
 
     return (
         <InputBase id={id} label={label}>
             <textarea
                 id={id}
-                className="w-full rounded-md border-gray-300 shadow-sm focus:border-orange-500 focus:ring-orange-500"
+                {...register}
+                className="mb-4 w-full rounded-md border-gray-300 shadow-sm focus:border-orange-500 focus:ring-orange-500"
                 rows={4}
                 placeholder={placeholder}
             />
