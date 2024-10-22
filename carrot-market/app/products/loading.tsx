83198bfb81ab188b07458db1f471bbb584088d1a
umingming <u_0112@naver.com>
2024-10-22 21:00:38 +0900

    feat: Add skeletons

---
+++ carrot-market/app/products/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+    return (
+        <div>
+            <div className="size-28 rounded-md bg-blue-700"></div>
+            <div></div>
+        </div>
+    );
+}
